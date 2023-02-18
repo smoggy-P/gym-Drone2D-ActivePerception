da9f3e3cf7607b5c518ac4e0d1090455f5bad9d0
siyuan <m.shi-5@student.tudelft.nl>
2023-02-18 23:40:38 +0100

    update main

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def myfun(gaze_method, agent_number, pillar_number, agent_speed, drone_speed, pl
         'drone_radius':5,
         'drone_max_yaw_speed':80,
         'drone_view_depth' : 80,
-        'drone_view_range': 360,
+        'drone_view_range': 90,
         'img_dir':img_dir,
         'max_steps':1000,
 
@@ -49,7 +49,7 @@ def myfun(gaze_method, agent_number, pillar_number, agent_speed, drone_speed, pl
 
 
 if __name__ == '__main__':
-    gaze_methods = ['NoControl']
+    gaze_methods = ['Oxford']
     planners = ['Jerk_Primitive']
     agent_numbers = [5, 10, 15]
     pillar_numbers = [0, 5, 10]
