7e37ff0cff86a8865212ed1822f6c3e161527d61
smoggy-P <smoggy@alumni.sjtu.edu.cn>
2023-04-04 19:58:36 +0200

    bug not solved

--- test.py
+++ test.py
@@ -11,12 +11,12 @@ import matplotlib.animation as animation
 # system
 dt = 0.1
 target = np.array([50, 50, 0, 0])
-num_agent = 5
+num_agent = 1
 
-agent_list = np.array([[0, 50, 5, 8, -8], #px, py, r, vx, vy
+agent_list = np.array([[30, 30,5,0, 0],
+                       [0, 50, 10, 8, -8], #px, py, r, vx, vy
                        [0, 50, 0, 10, -10],
                        [30, 30, 0, -10, -13],
-                       [0, 0, 0, 0, 0],
                        [0, 0, 0, 0, 0]])
 
 
@@ -41,21 +41,18 @@ R = np.eye(nu)
 lam = 500
 
 
-umin = -40 * np.ones([nu])
-umax = 40 * np.ones([nu])
+umin = -40000 * np.ones([nu])
+umax = 40000 * np.ones([nu])
 xmin = np.array([-200, -200, -10, -10])
 xmax = np.array([200, 200, 10, 10])
 
 # FORCESPRO multistage form
 # assume variable ordering z[i] = [si, ui, x_i+1] for i=0...N-1
 model = forcespro.nlp.SymbolicModel(N)
-model.nvar = 7  # number of stage variables
-model.neq = 4   # number of equality constraints
+model.nvar = ns+nu+nx  # number of stage variables
+model.neq = nx   # number of equality constraints
 model.nh = num_agent    # number of nonlinear inequality constraints
 model.npar = 5*num_agent  # number of parameters
-
-
-model.objective = lambda z: casadi.reshape(z[ns+nu:]-target,1,-1) @ Q @ casadi.vertcat(z[ns+nu:]-target) + lam*z[0]
 
 model.lb = np.concatenate(([0]*ns, umin, xmin), 0)
 model.ub = np.concatenate(([+float("inf")]*ns, umax, xmax), 0)
@@ -63,14 +60,17 @@ model.ub = np.concatenate(([+float("inf")]*ns, umax, xmax), 0)
 model.eq = lambda z: A @ casadi.reshape(z[ns+nu:],-1,1) + B @ casadi.reshape(z[ns:ns+nu],-1,1)
 
 for i in range(N):
+    if i < N-1:
+        model.objective[i] = lambda z: 0
+    else:
+        model.objective[i] = lambda z: casadi.reshape(z[ns+nu:]-target,1,-1) @ Q @ casadi.reshape(z[ns+nu:]-target,-1,1)
     def neq(z, p):
         p_reshaped = (p.reshape((5, -1))).T
-        print(p_reshaped)
-        lis = [(z[ns+nu]-p_reshaped[j,0]-p_reshaped[j,3]*i*dt)**2 + (z[ns+nu+1]-p_reshaped[j,1]-p_reshaped[j,4]*i*dt)**2+z[0]-p_reshaped[j,2]**2 for j in range(num_agent)]
+        lis = [(z[ns+nu]-p_reshaped[j,0])**2 + (z[ns+nu+1]-p_reshaped[j,1])**2-p_reshaped[j,2]**2 for j in range(num_agent)]
         return casadi.vertcat(lis[0], lis[1], lis[2], lis[3], lis[4])
         # return casadi.vertcat((z[3] - p[0] - p[2]*i*dt )**2 + (z[4] - p[1]- p[3]*i*dt)**2 + z[0] - agent_radius ** 2)
-    # model.ineq[i] = lambda z, p: casadi.vertcat((z[3] - p[0] - p[3]*i*dt )**2 + (z[4] - p[1]- p[4]*i*dt)**2 + z[0] - p[2] ** 2)
-    model.ineq[i] = neq
+    model.ineq[i] = lambda z, p: casadi.vertcat((z[3] - p[0] - p[3]*i*dt )**2 + (z[4] - p[1]- p[4]*i*dt)**2 + z[0])
+    # model.ineq[i] = neq
     model.hu[i] = [+float("inf")]*num_agent                 
     model.hl[i] = [0]*num_agent
 
@@ -90,7 +90,7 @@ options = forcespro.CodeOptions()
 options.printlevel = 0
 options.overwrite = 1
 options.nlp.bfgs_init = None
-options.maxit = 200
+options.maxit = 2000
 
 # generate code
 solver = model.generate_solver(options)
@@ -113,15 +113,16 @@ for k in range(kmax):
     problem["xinit"] = x[:, k]
 
     params = []
-
-    for agent in agent_list:
-        params.extend([agent[0]+k*dt*agent[3], agent[1]+k*dt*agent[4], agent[2], agent[3], agent[4]])
+    for i, agent in enumerate(agent_list):
+        if i < num_agent:
+            params.extend([agent[0]+k*dt*agent[3], agent[1]+k*dt*agent[4], agent[2], agent[3], agent[4]])
 
     problem["all_parameters"] = np.tile(np.array(params), (model.N,))
 
     # call the solver
     solverout, exitflag, info = solver.solve(problem)
     if exitflag < 0:
+        print(exitflag)
         break
 
     s[0, k] = solverout["x01"][0]
@@ -162,7 +163,7 @@ def update(step):
 
     
     lb = min(min(px),min(py)-10,-10)
-    ub = max(max(px),max(py)+10,60)
+    ub = max(max(px),max(py)+10, 60)
     ax.set_xlim(lb, ub)
     ax.set_ylim(lb, ub)
     return line, 
@@ -175,4 +176,4 @@ ani = animation.FuncAnimation(fig = fig,
                               frames = x.shape[1], 
                               interval = 100)
 plt.show()
-
+print(s)
