16eed5fbdb00e9c37b9a542c4efa1cd7d4572a01
smoggy-P <smoggy@alumni.sjtu.edu.cn>
2023-04-06 02:00:25 +0200

    add test results

--- test.py
+++ test.py
@@ -13,8 +13,8 @@ dt = 0.1
 target = np.array([50, 50, 0, 0])
 num_agent = 1
 
-agent_list = np.array([[30, 30,5,0, 0],
-                       [0, 50, 10, 8, -8], #px, py, r, vx, vy
+agent_list = np.array([[0, 50, 10, 10, -10],
+                       [0, 50, 0, 8, -8], #px, py, r, vx, vy
                        [0, 50, 0, 10, -10],
                        [30, 30, 0, -10, -13],
                        [0, 0, 0, 0, 0]])
@@ -22,27 +22,26 @@ agent_list = np.array([[30, 30,5,0, 0],
 
 
 A = np.array([[1, 0, dt, 0],
-            [0, 1, 0, dt],
-            [0, 0, 1, 0],
-            [0, 0, 0, 1]])
-B = np.array([[0.5*dt**2, 0],
-            [0, 0.5*dt**2],
-            [dt, 0],
-            [0, dt]])
+              [0, 1, 0, dt],
+              [0, 0, 1, 0],
+              [0, 0, 0, 1]])
+B = np.array([[0.5*(dt**2), 0],
+              [0, 0.5*(dt**2)],
+              [dt, 0],
+              [0, dt]])
 
 nx = 4
 nu = 2
 ns = 1
 
 # MPC setup
-N = 10
+N = 20
 Q = np.eye(nx)
-R = np.eye(nu)
-lam = 500
+lam = 50000
 
 
-umin = -40000 * np.ones([nu])
-umax = 40000 * np.ones([nu])
+umin = -40 * np.ones([nu])
+umax = 40 * np.ones([nu])
 xmin = np.array([-200, -200, -10, -10])
 xmax = np.array([200, 200, 10, 10])
 
@@ -61,17 +60,16 @@ model.eq = lambda z: A @ casadi.reshape(z[ns+nu:],-1,1) + B @ casadi.reshape(z[n
 
 for i in range(N):
     if i < N-1:
-        model.objective[i] = lambda z: 0
+        model.objective[i] = lambda z: lam* z[0]
     else:
-        model.objective[i] = lambda z: casadi.reshape(z[ns+nu:]-target,1,-1) @ Q @ casadi.reshape(z[ns+nu:]-target,-1,1)
-    def neq(z, p):
-        p_reshaped = (p.reshape((5, -1))).T
-        lis = [(z[ns+nu]-p_reshaped[j,0])**2 + (z[ns+nu+1]-p_reshaped[j,1])**2-p_reshaped[j,2]**2 for j in range(num_agent)]
-        return casadi.vertcat(lis[0], lis[1], lis[2], lis[3], lis[4])
-        # return casadi.vertcat((z[3] - p[0] - p[2]*i*dt )**2 + (z[4] - p[1]- p[3]*i*dt)**2 + z[0] - agent_radius ** 2)
-    model.ineq[i] = lambda z, p: casadi.vertcat((z[3] - p[0] - p[3]*i*dt )**2 + (z[4] - p[1]- p[4]*i*dt)**2 + z[0])
-    # model.ineq[i] = neq
-    model.hu[i] = [+float("inf")]*num_agent                 
+        model.objective[i] = lambda z: casadi.reshape(z[ns+nu:]-target,1,-1) @ Q @ casadi.reshape(z[ns+nu:]-target,-1,1) + lam*z[0]
+    # def neq(z, p):
+    #     p_reshaped = (p.reshape((5, -1))).T
+    #     lis = [(z[ns+nu]-p_reshaped[j,0])**2 + (z[ns+nu+1]-p_reshaped[j,1])**2-p_reshaped[j,2]**2 for j in range(num_agent)]
+    #     return casadi.vertcat(lis[0], lis[1], lis[2], lis[3], lis[4])
+    #     # return casadi.vertcat((z[3] - p[0] - p[2]*i*dt )**2 + (z[4] - p[1]- p[3]*i*dt)**2 + z[0] - agent_radius ** 2)
+    model.ineq[i] = lambda z, p: (z[3]-p[0]-(p[3]*(i)*dt))**2 + (z[4]-p[1]-(p[4]*(i)*dt))**2 -(p[2]**2) + z[0]
+    model.hu[i] = [+float("inf")]*num_agent
     model.hl[i] = [0]*num_agent
 
 model.E = np.concatenate([np.zeros((4, 3)), np.eye(4)], axis=1) 
@@ -91,6 +89,15 @@ options.printlevel = 0
 options.overwrite = 1
 options.nlp.bfgs_init = None
 options.maxit = 2000
+options.printlevel  = 1
+                             
+options.optlevel    = 3
+options.overwrite   = 1
+options.cleanup     = 1
+options.timing      = 1
+options.parallel    = 1
+options.threadSafeStorage = True
+options.noVariableElimination = 0
 
 # generate code
 solver = model.generate_solver(options)
@@ -99,7 +106,7 @@ solver = model.generate_solver(options)
 # --------------
 
 x1 = [0, 0, 0, 0]
-kmax = 50
+kmax = 100
 x = np.zeros((nx, kmax + 1))
 x[:, 0] = x1
 u = np.zeros((nu, kmax))
@@ -109,13 +116,17 @@ problem = {}
 solvetime = []
 iters = []
 
+fig = plt.figure(figsize=(8,8))
+ax = plt.axes()
+ax.set_xlim(-10, 60)
+ax.set_ylim(-10, 60)
+plt.grid()
+plt.ion()
+
 for k in range(kmax):
     problem["xinit"] = x[:, k]
 
-    params = []
-    for i, agent in enumerate(agent_list):
-        if i < num_agent:
-            params.extend([agent[0]+k*dt*agent[3], agent[1]+k*dt*agent[4], agent[2], agent[3], agent[4]])
+    params = [agent_list[0,0]+k*dt*agent_list[0,3], agent_list[0,1]+k*dt*agent_list[0,4], agent_list[0,2], agent_list[0,3], agent_list[0,4]]
 
     problem["all_parameters"] = np.tile(np.array(params), (model.N,))
 
@@ -129,6 +140,7 @@ for k in range(kmax):
     u[0, k] = solverout["x01"][1]
     u[1, k] = solverout["x01"][2]
 
+
     solvetime.append(info.solvetime)
     iters.append(info.it)
     c = np.concatenate([s[:, k], u[:, k], x[:, k]])
@@ -136,44 +148,81 @@ for k in range(kmax):
     b = a.full()
     x[:, k + 1] = b.reshape(nx,)
 
-
-fig = plt.figure(figsize=(8,8))
-ax = plt.axes()
-px = []
-py = []
-line, = plt.plot(px, py)
-plt.grid()
-
-
-def init():
-    px.clear()
-    py.clear()
-    line.set_data(px, py)    
-    return line,
-
-def update(step):
-    px.append(x[0, step])
-    py.append(x[1, step])
-    line.set_data(px, py)
-
+    circle1 = plt.Circle((params[0], params[1]), params[2], color='lightgrey')
+    circle2 = plt.Circle((x[0,k+1],x[1,k+1]), 0.3, color='r')
+    
     ax.patches = []
-    for agent in agent_list:
-        circle = plt.Circle((agent[0] + step*dt*agent[3], agent[1] + step*dt*agent[4]), agent[2], color='lightgrey')
-        ax.add_patch(circle)
+    ax.add_patch(circle1)
+    ax.add_patch(circle2)
+
+    # ax.scatter([solverout["x01"][3],
+    #           solverout["x02"][3],
+    #           solverout["x03"][3],
+    #           solverout["x04"][3],
+    #           solverout["x05"][3],
+    #           solverout["x06"][3],
+    #           solverout["x07"][3],
+    #           solverout["x08"][3],
+    #           solverout["x09"][3],
+    #           solverout["x10"][3],
+    #           solverout["x11"][3],
+    #           solverout["x12"][3],
+    #           solverout["x13"][3],
+    #           solverout["x14"][3]],[solverout["x01"][4],
+    #                              solverout["x02"][4],
+    #                              solverout["x03"][4],
+    #                              solverout["x04"][4],
+    #                              solverout["x05"][4],
+    #                              solverout["x06"][4],
+    #                              solverout["x07"][4],
+    #                              solverout["x08"][4],
+    #                              solverout["x09"][4],
+    #                              solverout["x10"][4],
+    #                              solverout["x11"][4],
+    #                              solverout["x12"][4],
+    #                              solverout["x13"][4],
+    #                              solverout["x14"][4]], s=0.2)
+    plt.pause(0.1)
+    
+
+
+# fig = plt.figure(figsize=(8,8))
+# ax = plt.axes()
+# px = []
+# py = []
+# line, = plt.plot(px, py)
+# plt.grid()
+
+
+# def init():
+#     px.clear()
+#     py.clear()
+#     line.set_data(px, py)    
+#     return line,
+
+# def update(step):
+#     px.append(x[0, step])
+#     py.append(x[1, step])
+#     line.set_data(px, py)
+
+#     ax.patches = []
+#     for agent in agent_list:
+#         circle = plt.Circle((agent[0] + step*dt*agent[3], agent[1] + step*dt*agent[4]), agent[2], color='lightgrey')
+#         ax.add_patch(circle)
 
     
-    lb = min(min(px),min(py)-10,-10)
-    ub = max(max(px),max(py)+10, 60)
-    ax.set_xlim(lb, ub)
-    ax.set_ylim(lb, ub)
-    return line, 
+#     lb = min(min(px),min(py)-10,-10)
+#     ub = max(max(px),max(py)+10, 60)
+#     ax.set_xlim(lb, ub)
+#     ax.set_ylim(lb, ub)
+#     return line, 
 
-
-ani = animation.FuncAnimation(fig = fig, 
-                              func = update,
-                              init_func = init,
-                              blit = False,
-                              frames = x.shape[1], 
-                              interval = 100)
-plt.show()
 print(s)
+
+# ani = animation.FuncAnimation(fig = fig, 
+#                               func = update,
+#                               init_func = init,
+#                               blit = False,
+#                               frames = x.shape[1], 
+#                               interval = 100)
+# plt.show()
