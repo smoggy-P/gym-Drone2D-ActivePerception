f35173d32ec61b96505ed6273f16f471389021ee
smoggy-P <smoggy@alumni.sjtu.edu.cn>
2023-05-27 20:15:26 +0200

    update main for nocontrol

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ result_dir = './experiment/results_'+str(datetime.now())+'.csv'
 
 if __name__ == '__main__':
 
-    gaze_methods = ['Owl']
+    gaze_methods = ['NoControl']
     planners = ['Primitive']
 
     # Environment difficulty
     motion_profiles = ['CVM']
     agent_numbers = [10, 20, 30]
-    agent_sizes = [5, 10, 15]
+    agent_sizes = [10, 15]
     agent_max_speeds = [20, 40, 60]
     map_ids = range(5)
     pillar_numbers = [0]
 
     # Problem difficulty
-    drone_max_speeds = [40]
+    drone_max_speeds = [60]
     var_depths = [0]
     
 
@@ -34,7 +34,7 @@ if __name__ == '__main__':
     for (gaze_method, planner, motion_profile, var_depth,
         agent_number, pillar_number, agent_speed,
         drone_speed, agent_size, map_id) in params:
-        axis_range = [25, 250, 475]
+        axis_range = [40, 250, 460]
         for start_pos, target_pos in vis.product(product(axis_range, axis_range), product(axis_range, axis_range)):
             if start_pos != target_pos:
                 cfg = Params(debug=False,
@@ -49,7 +49,8 @@ if __name__ == '__main__':
                             agent_radius=agent_size,
                             map_id=map_id,
                             init_pos=start_pos,
-                            target_list=[target_pos])
+                            target_list=[target_pos],
+                            drone_view_range=360)
                 experiment = Experiment(cfg, result_dir)
                 experiment.run()
         
